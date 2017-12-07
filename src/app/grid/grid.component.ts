4a4036e8761338cfe9373c9dbfe2d5ecdf4aae77
filipemoraisjorge-zoef <filipe.jorge@zoef.com>
2017-12-07 15:49:11 +0100

    new sort algorithm

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -16,6 +16,7 @@ export class GridComponent implements OnInit {
     { content: {}, text: '2x1', width: 2, height: 1, color: 'lightpink' },
     { content: {}, text: '1x2', width: 1, height: 2, color: '#DDBDF1' },
   ];
+
   private baseContent = [
     { title: '1', text: 'The first  item' },
     { title: '2', text: 'The second  item' },
@@ -23,49 +24,61 @@ export class GridComponent implements OnInit {
     { title: '4', text: 'The fourth  item' },
     { title: '5', text: 'The fifth  item' },
     { title: '6', text: 'The sixth random item' },
-
   ];
+
   public tiles = [];
+  public tiles2 = [];
+  public tiles3 = [];
   public unsortedTiles = [];
 
   public map: string[];
 
-  private GRID_QTY_ELEMENTS = 40;
-  private MAX_SIZE_MULTI = 1;
+  private GRID_QTY_ELEMENTS = 10000;
   public MAX_COLS = 8;
-
+  private newItemCounter = 0;
   public isLoaded = false;
-  constructor(private masonrySortService: MasonrySortService) {
-    masonrySortService.maxColumns = this.MAX_COLS;
-   }
+  constructor(private masonrySortService: MasonrySortService) { }
 
   ngOnInit() {
-     // mock/fill the array with random items;
-    const unsortedTiles = [];
+    // mock/fill the array with random items;
+    let unsortedTiles = [];
     for (let i = 0; i < this.GRID_QTY_ELEMENTS; i++) {
-
-      // const size = this.random(1, this.MAX_SIZE_MULTI);
-      const index = this.random(0, this.baseTiles.length - 1);
-      const contentIndex = this.random(0, this.baseContent.length - 1);
-
-      const element = { ...this.baseTiles[index] };
-
-      // element.cols = element.cols * size;
-      // element.rows = element.rows * size;
-      element.content = { title: i }; // this.baseContent[contentIndex];
-      element.text = `${i + 1}`; // - ${element.cols}x${element.rows}`;
-
+      //  const contentIndex = this.random(0, this.baseContent.length - 1);
+      const element = this.getRandomItem(`${i + 1}`);
       unsortedTiles.push(element);
-      //      this.tiles.push(i === bigIndex ? this.bigTile : element);
     }
+
+    // unsortedTiles = [
+    //   { ...this.baseTiles[0] },
+    //   { ...this.baseTiles[2] },
+    //   { ...this.baseTiles[2] },
+    //   { ...this.baseTiles[1] },
+    //   { ...this.baseTiles[3] },
+    //   { ...this.baseTiles[0] },
+    //   { ...this.baseTiles[0] },
+    //   { ...this.baseTiles[1] },
+    // ];
+    // unsortedTiles.map((item, i) => {
+    //   item.text = `${i + 1}`;
+    //   return item;
+    // });
 
     this.unsortedTiles = unsortedTiles;
     // sort in a way that mat-grid-list doesn't show empty spaces;
 
-    const start = performance.now();
-    this.tiles = this.masonrySortService.sort(unsortedTiles);
-    const end = performance.now();
-    console.log((end - start) / 1000);
+    this.tiles3 = this.masonrySortService.sort3(unsortedTiles, this.MAX_COLS);
+
+
+    // console.time('sort2');
+    // this.tiles2 = this.masonrySortService.sort2(unsortedTiles, this.MAX_COLS);
+    // console.timeEnd('sort2');
+
+    this.tiles = this.masonrySortService.sort(unsortedTiles, this.MAX_COLS);
+
+    // console.log('unsort', this.unsortedTiles.map(item => item.text));
+    // console.log('tiles ', this.tiles.map(item => item.text));
+    // console.log('tiles2', this.tiles2.map(item => item.text));
+
     this.isLoaded = true;
   }
 
@@ -74,5 +87,23 @@ export class GridComponent implements OnInit {
     return min + Math.round(Math.random() * (max - min));
   }
 
+  public addItem() {
+    const element = this.getRandomItem('n' + (this.newItemCounter++));
+    this.unsortedTiles.unshift(element);
+    const unsrt = [...this.unsortedTiles];
+    this.tiles = this.masonrySortService.sort(unsrt, this.MAX_COLS);
+    this.tiles3 = this.masonrySortService.sort3(unsrt, this.MAX_COLS);
+  }
+
+  public sort(arr: Item[]) {
+    return this.masonrySortService.sort(arr, this.MAX_COLS);
+  }
+
+  private getRandomItem(label: string) {
+    const index = this.random(0, this.baseTiles.length - 1);
+    const item = { ...this.baseTiles[index] };
+    item.text = label;
+    return item;
+  }
 
 }
--- src/app/services/masonry-sort.service.ts
+++ src/app/services/masonry-sort.service.ts
@@ -3,34 +3,40 @@ import { Item } from '../interfaces/item';
 
 @Injectable()
 export class MasonrySortService {
-  public maxColumns: number;
+
 
   public map: string[] = [];
-
-  public sort(arr: Item[]): Item[] {
-    const result = [];
+  public sort(arr: Item[], maxColumns): Item[] {
+    this.map = [];
+    console.time('sort1');
     let occupiedCells = new Set<number>();
+    const freeCells: number[] = [];
+
     const sortedSet = new Set<Item>();
 
-    let insertCell = 0;
     // iterate all items
     for (let i = 0; i < arr.length; i++) {
       const item = arr[i];
 
-      // find first free
-      let firstFreeCell = 0;
-      while (occupiedCells.has(firstFreeCell)) {
-        firstFreeCell++;
-      }
+      // first free
+      const firstFreeCell = freeCells.shift() || 0;
 
-      insertCell = firstFreeCell;
+      let insertCell = firstFreeCell;
 
-      while (!this.fits(item, insertCell, occupiedCells, this.maxColumns)) {
+      // console.log('item: ', item.text, 'start', insertCell);
+
+      while (!this.fits(item, insertCell, occupiedCells, maxColumns)) {
+        if (!occupiedCells.has(insertCell)) { freeCells.push(insertCell); }
         insertCell++;
+        // console.log('++', insertCell);
+      }
+      occupiedCells = this.occupyCells(item, insertCell, occupiedCells, maxColumns);
+      const nextCell = insertCell + item.width;
+      if (!occupiedCells.has(nextCell)) {
+        freeCells.push(insertCell + item.width);
       }
 
-      occupiedCells = this.occupyCells(item, insertCell, occupiedCells, this.maxColumns);
-      this.debugMap();
+      // console.log('free', freeCells);
     }
 
     for (const cell of this.map) {
@@ -39,17 +45,137 @@ export class MasonrySortService {
         sortedSet.add(item);
       }
     }
-    return Array.from(sortedSet);
+    const result = Array.from(sortedSet);
+    console.timeEnd('sort1');
+    // this.debugMap(maxColumns, this.map);
+    return result;
   }
 
-  private fits(item: Item, startCell: number, occupiedCells: Set<number>, maxCols: number): boolean {
+  public sort2(arr: Item[], maxColumns: number): Item[] {
+    // iterate all items
+    const sorted = [];
+    let occupiedCells = new Set<number>();
+    let firstEmptyIndex = 0;
+    let insertCell = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      const item = arr[i];
+
+      if (!this.fits(item, insertCell, occupiedCells, maxColumns)) {  // if it doesn't fit
+        // console.log(item.text, '++');
+        sorted.push(item);
+      } else { // if it fits
+        if (sorted[firstEmptyIndex] !== undefined) { // if it's already filled
+          const endPart = sorted.splice(firstEmptyIndex);
+          sorted.push(item);
+          sorted.push(...endPart);
+          firstEmptyIndex++;
+        } else { // if it's free to insert;
+          sorted[firstEmptyIndex] = item;
+        }
+        firstEmptyIndex++;
+        insertCell += item.width;
+        // console.log(item.text, firstEmptyIndex);
+      }
+      occupiedCells = this.occupyCells(item, insertCell, occupiedCells, maxColumns);
+      // console.log(sorted.map(i => i.text));
+    }
+    return sorted;
+  }
+
+  public sort3(arr: Item[], maxColumns: number): Item[] {
+    console.time('sort3');
+    const sorted = new Set<Item>();
+    let map: any[][] = [new Array(maxColumns).fill('')]; // start with an empty row;
+    let cell = 0;
+    const unsorted: Item[] = [...arr];
+
+    while (unsorted.length > 0) {
+      let item;
+      // find a freeCell, what to do when there are no free cells?
+      if (cell >= map.length * maxColumns) { // bigger than the current map cells
+        // add to one more row to map
+        map.push(new Array(maxColumns).fill(''));
+      }
+      while (!this.isCellFree(cell, map, maxColumns)) {
+        cell++;
+        if (cell >= map.length * maxColumns) { // bigger than the current map cells
+          // add to one more row to map
+          map.push(new Array(maxColumns).fill(''));
+        }
+      }
+
+      // iterate unsorted once
+      for (let i = 0; i < unsorted.length; i++) {
+        item = unsorted[i];
+        // find one item that fits
+        if (this.fits3(item, cell, map, maxColumns)) {
+          // find item on unsorted and take it;
+          // more code but more efficient than shift/unshift that are always O(n);
+          const index = unsorted.findIndex(it => it === item);
+          if (index > -1) {
+            unsorted.splice(index, 1);
+          }
+          break;
+        }
+      }
+
+      if (item !== undefined) {
+        // it fits, place item
+        map = this.occupyCells3(item, cell, map, maxColumns);
+        sorted.add(item);
+        cell += item.width;
+      } else {
+        // did not find any, leave the cell empty
+        cell++;
+      }
+     // this.debugMap3(maxColumns, map);
+    }
+    const result = Array.from(sorted);
+    console.timeEnd('sort3');
+    return result;
+
+  }
+
+  private fits3(item: Item, startCell: number, map: any[][], maxColumns: number): boolean {
     // if is on edge
-    const outOfLimits = (startCell % maxCols) + (item.width - 1) > (maxCols - 1);
+    const outOfLimits = (startCell % maxColumns) + (item.width - 1) > (maxColumns - 1);
+
+    const startCellRow = this.getRow(startCell, maxColumns);
+    const startCellCol = this.getCol(startCell, maxColumns);
 
     // if one of the cells is occupied;
     let isEmpty = true;
     if (!outOfLimits) {
-      const itemCells = this.getCells(item, startCell, maxCols);
+      for (let itemRow = 0; itemRow < item.height; itemRow++) {
+        for (let itemCol = 0; itemCol < item.width; itemCol++) {
+          const row = startCellRow + itemRow;
+          const col = startCellCol + itemCol;
+
+          // if they don't exist, initialize them
+          if (map[row] === undefined) {
+            map[row] = new Array(maxColumns).fill('');
+          }
+
+          // check if are free; empty string means free.
+          if (map[row][col] !== '') {
+            isEmpty = false;
+            break;
+          }
+        }
+      }
+    }
+    return !outOfLimits && isEmpty;
+  }
+
+  private fits(item: Item, startCell: number, occupiedCells: Set<number>, maxColumns: number): boolean {
+    // if is on edge
+    const outOfLimits = (startCell % maxColumns) + (item.width - 1) > (maxColumns - 1);
+
+    // if one of the cells is occupied;
+    let isEmpty = true;
+    if (!outOfLimits) {
+      const itemCells = this.getCells(item, startCell, maxColumns);
       for (const cell of itemCells) {
         if (occupiedCells.has(cell)) {
           isEmpty = false;
@@ -60,20 +186,39 @@ export class MasonrySortService {
     return !outOfLimits && isEmpty;
   }
 
-  private occupyCells(item: Item, startCell: number, occupiedCells: Set<number>, maxCols: number): Set<number> {
-    const itemCells = this.getCells(item, startCell, maxCols, HTMLOptGroupElement);
+  private occupyCells3(item: Item, startCell: number, map: any[][], maxColumns: number): any[][] {
+    const startCellRow = this.getRow(startCell, maxColumns);
+    const startCellCol = this.getCol(startCell, maxColumns);
+
+    for (let row = startCellRow; row < startCellRow + item.height; row++) {
+      for (let col = startCellCol; col < startCellCol + item.width; col++) {
+
+        map[row][col] = item.text;
+      }
+    }
+    return map;
+  }
+
+  private occupyCells(item: Item, startCell: number, occupiedCells: Set<number>, maxColumns: number): Set<number> {
+    const itemCells = this.getCells(item, startCell, maxColumns, true);
     for (const cell of itemCells) {
       occupiedCells.add(cell);
     }
     return occupiedCells;
   }
 
-  private getCells(item: Item, startCell, maxCols: number, o?: boolean): number[] {
+  private isCellFree(cell: number, map: any[][], maxColumns: number): boolean {
+    const row = this.getRow(cell, maxColumns);
+    const col = this.getCol(cell, maxColumns);
+    return map[row][col] === '';
+  }
+
+  private getCells(item: Item, startCell, maxColumns: number, o?: boolean): number[] {
 
     const cells = [];
     for (let row = 0; row < item.height; row++) {
       for (let column = 0; column < item.width; column++) {
-        const cell = startCell + column + row * maxCols;
+        const cell = startCell + column + row * maxColumns;
         cells.push(cell);
 
         if (o) {
@@ -84,15 +229,45 @@ export class MasonrySortService {
     return cells;
   }
 
-  private debugMap() {
+  private getRow(cell: number, maxColumns: number): number {
+    return Math.floor(cell / maxColumns);
+  }
+
+  private getCol(cell: number, maxColumns: number): number {
+    return cell % maxColumns;
+  }
+
+  private geFirstCellOfRow(row: number, maxColumns: number) {
+    return (row * maxColumns);
+  }
+
+  private getLastCellOfRow(row: number, maxColumns: number) {
+    return this.geFirstCellOfRow(row, maxColumns) + (maxColumns - 1);
+  }
+
+  private debugMap(maxColumns, map) {
     let mapStr = '|';
-    for (let index = 0; index < this.map.length; index++) {
+    for (let index = 0; index < map.length; index++) {
       let cell = this.map[index] || '';
       cell = cell.length < 2 ? ' '.repeat(2 - cell.length) + cell : cell;
-      mapStr += ` ${cell}${(index + 1) % this.maxColumns === 0 ? ' |\n|' : ' |'}`;
+      mapStr += ` ${cell}${(index + 1) % maxColumns === 0 ? ' |\n|' : ' |'}`;
     }
+    console.log('map1');
     console.log(mapStr);
   }
 
-
+  private debugMap3(maxColumns, map) {
+    let mapStr = '|';
+    for (let row = 0; row < map.length; row++) {
+      for (let column = 0; column < map[0].length; column++) {
+        let cell = map[row][column] || '';
+        cell = cell.toString();
+        cell = cell.length < 2 ? ' '.repeat(2 - cell.length) + cell : cell;
+        mapStr += ` ${cell} |`;
+      }
+      mapStr += '\n|';
+    }
+    console.log('map3');
+    console.log(mapStr);
+  }
 }
